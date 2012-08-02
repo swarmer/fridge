d2dbfa65fdeddcb6d2ff700151a4edd0cec42121
Anton Barkovsky <swarmer.pm@gmail.com>
2012-08-02 15:01:24 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(name='Fridge',
+        version='0.1',
+        py_modules=['fridge'],
+        description='Persistent JSON-encoded distionary',
+        author='Anton Barkovsky',
+        author_email='swarmer.pm@gmail.com',
+        url='http://fridge.readthedocs.org/')
