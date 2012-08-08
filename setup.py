a61b79a6d427745f3bf240554b3dd852d8e6ed65
Anton Barkovsky <swarmer.pm@gmail.com>
2012-08-08 11:47:29 +0300

    Add classifiers

--- setup.py
+++ setup.py
@@ -7,4 +7,14 @@ setup(name='Fridge',
         description='Persistent JSON-encoded distionary',
         author='Anton Barkovsky',
         author_email='swarmer.pm@gmail.com',
-        url='http://fridge.readthedocs.org/')
+        url='http://fridge.readthedocs.org/',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Topic :: Software Development :: Libraries :: Python Modules'])
