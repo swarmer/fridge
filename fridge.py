145d13cbbdd90e2ab1380b4a1b42945e965b3565
Anton Barkovsky <swarmer.pm@gmail.com>
2012-08-10 00:38:19 +0300

    ENGLISH MOTHERFUCKER DO YOU SPEAK IT

--- fridge.py
+++ fridge.py
@@ -27,7 +27,7 @@ class Fridge(dict):
     def readonly(cls, *args, **kwargs):
         """
         Return an already closed read-only instance of Fridge.
-        Arguments are the same as for constructor.
+        Arguments are the same as for the constructor.
         """
         fridge = cls(*args, **kwargs)
         fridge.close()
