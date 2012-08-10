9ba9df65ebf9ff8322c63f9317048991869f7a10
Anton Barkovsky <swarmer.pm@gmail.com>
2012-08-10 14:46:26 +0300

    Use github pages instead

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='Fridge',
         description='Persistent JSON-encoded dictionary',
         author='Anton Barkovsky',
         author_email='swarmer.pm@gmail.com',
-        url='https://github.com/swarmer/fridge',
+        url='http://swarmer.github.com/fridge/',
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
